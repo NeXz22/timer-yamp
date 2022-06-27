3f61f19115c4d76192e4ef3877c01237f0dfec7f
Thomas Sikora <thomas-sikora@web.de>
2022-06-27 20:34:27 +0200

    Setting dark-mode based on os-preference

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,19 +1,35 @@
-import {Component} from '@angular/core';
+import {AfterViewInit, Component, ElementRef, OnInit, ViewChild} from '@angular/core';
 
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-    title = 'frontend';
+export class AppComponent implements OnInit, AfterViewInit {
 
-    onThemeSwitchChange($event: Event): void {
-        const themeCheckbox = $event.target as HTMLInputElement;
-        if (themeCheckbox.checked) {
+    @ViewChild('themeCheckbox') themeCheckboxRef!: ElementRef<HTMLInputElement>;
+
+    isDarkModePreference: boolean = false;
+
+    onThemeSwitchChange(): void {
+        if (this.themeCheckboxRef.nativeElement.checked) {
             document.body.dataset['theme'] = 'dark';
         } else {
             document.body.dataset['theme'] = 'light';
+        }
+    }
+
+    ngOnInit(): void {
+        this.isDarkModePreference = window.matchMedia('(prefers-color-scheme: dark)').matches;
+
+        if (this.isDarkModePreference) {
+            document.body.dataset['theme'] = 'dark';
+        }
+    }
+
+    ngAfterViewInit(): void {
+        if (this.isDarkModePreference) {
+            this.themeCheckboxRef.nativeElement.checked = true;
         }
     }
 }

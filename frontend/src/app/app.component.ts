ae1f85420260d99ee143ef49de99d42000f28914
Thomas Sikora <thomas-sikora@web.de>
2022-06-27 19:14:40 +0200

    Adding support for multiple themes

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'frontend';
+    title = 'frontend';
+
+    onThemeSwitchChange($event: Event): void {
+        const themeCheckbox = $event.target as HTMLInputElement;
+        if (themeCheckbox.checked) {
+            document.body.dataset['theme'] = 'dark';
+        } else {
+            document.body.dataset['theme'] = 'light';
+        }
+    }
 }

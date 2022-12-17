ba0857c7fabc628ac6b9afb9b3dfb9f5f37aa5d4
Thomas Sikora <thomas-sikora@web.de>
2022-12-17 14:24:36 +0100

    Refactoring of socket event listeners

--- server/index.js
+++ server/index.js
@@ -48,97 +48,132 @@ function startServer(port) {
 function setupConnection() {
     io.on('connection', (socket) => {
         log(`[${socket.id}] connected. Number of currently connected sockets: ${io.engine.clientsCount}`);
-
-        socket.on('disconnecting', () => {
-            const sessionsToLeave = findSessionsToLeave(socket);
-            for (const session of sessionsToLeave) {
-                io.in(session).emit('watch changed', io.sockets.adapter.rooms.get(session).size - 1);
-            }
-        });
-
-        socket.on('disconnect', (reason) => {
-            log(`[${socket.id}] disconnected. Reason: ${reason}. Number of currently connected sockets: ${io.engine.clientsCount}`);
-        });
-
-        socket.on('join session', (sessionToJoin) => {
-            socket.join(sessionToJoin);
-            const message = `[${socket.id}] joined the session [${sessionToJoin}]`;
-            io.in(sessionToJoin).emit('to all clients', message);
-            io.in(sessionToJoin).emit('watch changed', io.sockets.adapter.rooms.get(sessionToJoin).size);
-            log(message);
-
-            if (sessionSettings.has(sessionToJoin)) {
-                if (sessionSettings.get(sessionToJoin)) {
-                    const existingSessionSettings = {...sessionSettings.get(sessionToJoin)};
-                    if (sessionSettings.get(sessionToJoin).countdownRunning) {
-                        existingSessionSettings.countdownLeft -= Date.now() - existingSessionSettings.timeCountdownStarted;
-                    }
-                    socket.emit('settings for requested session already exist', existingSessionSettings)
-                }
-            } else {
-                sessionSettings.set(sessionToJoin, {...defaultSessionSettings});
-            }
-        });
-
-        socket.on('participants changed', (participantsChange) => {
-            const message = `[${socket.id}] emitted new participants: [${participantsChange.participants}]`;
-            io.in(participantsChange.sessionId).emit('to all clients', message);
-            log(message);
-
-            sessionSettings.get(participantsChange.sessionId).participants = participantsChange.participants;
-
-            socket.broadcast.to(participantsChange.sessionId).emit('participants updated', participantsChange.participants);
-        });
-
-        socket.on('goals changed', (goalsChange) => {
-            const message = `[${socket.id}] emitted new goals: [${goalsChange.goals}]`;
-            io.in(goalsChange.sessionId).emit('to all clients', message);
-            log(message);
-
-            sessionSettings.get(goalsChange.sessionId).goals = goalsChange.goals;
-
-            socket.broadcast.to(goalsChange.sessionId).emit('goals updated', goalsChange.goals);
-        });
-
-        socket.on('start stop countdown', (s) => {
-            sessionSettings.get(s.sessionId).countdownRunning = !sessionSettings.get(s.sessionId).countdownRunning;
-            sessionSettings.get(s.sessionId).countdownLeft = s.timeLeft;
-            if (sessionSettings.get(s.sessionId).countdownRunning) {
-                sessionSettings.get(s.sessionId).timeCountdownStarted = Date.now();
-            }
-            io.in(s.sessionId).emit('countdown update', {
-                countdownRunning: sessionSettings.get(s.sessionId).countdownRunning,
-                countdownLeft: sessionSettings.get(s.sessionId).countdownLeft
-            });
-        });
-
-        socket.on('reset countdown', (s) => {
-            sessionSettings.get(s.sessionId).countdownLeft = s.timeLeft;
-            io.in(s.sessionId).emit('countdown update', {
-                countdownRunning: sessionSettings.get(s.sessionId).countdownRunning,
-                countdownLeft: sessionSettings.get(s.sessionId).countdownLeft
-            });
-        });
-
-        socket.on('countdown ended', (s) => {
-            sessionSettings.get(s.sessionId).countdownRunning = false;
-            sessionSettings.get(s.sessionId).countdownLeft = s.timeLeft;
-        });
-
-        socket.on('time seconds settings changed', (s) => {
-            sessionSettings.get(s.sessionId).desiredSeconds = s.desiredSeconds;
-            io.in(s.sessionId).emit('seconds changed', {
-                desiredSeconds: s.desiredSeconds
-            });
-        });
-
-        socket.on('time minutes settings changed', (s) => {
-            sessionSettings.get(s.sessionId).desiredMinutes = s.desiredMinutes;
-            io.in(s.sessionId).emit('minutes changed', {
-                desiredMinutes: s.desiredMinutes
-            });
-        });
+        socket.on('disconnecting', onDisconnecting(socket));
+        socket.on('disconnect', onDisconnect(socket));
+        socket.on('join session', onSessionJoined(socket));
+        socket.on('participants changed', onParticipantsChanged(socket));
+        socket.on('goals changed', onGoalsChanged(socket));
+        socket.on('start stop countdown', onStartStopCountdown());
+        socket.on('reset countdown', onResetCountdown());
+        socket.on('countdown ended', onCountdownEnded());
+        socket.on('time seconds settings changed', onTimeSecondsSettingsChanged());
+        socket.on('time minutes settings changed', onTimeMinutesSettingsChanged());
     });
+}
+
+function onDisconnecting(socket) {
+    return function () {
+        const sessionsToLeave = findSessionsToLeave(socket);
+        for (const session of sessionsToLeave) {
+            io.in(session).emit('watch changed', io.sockets.adapter.rooms.get(session).size - 1);
+        }
+    }
+
+}
+
+function onDisconnect(socket) {
+    return function (reason) {
+        log(`[${socket.id}] disconnected. Reason: ${reason}. Number of currently connected sockets: ${io.engine.clientsCount}`);
+    }
+}
+
+function onSessionJoined(socket) {
+    return function (sessionToJoin) {
+        socket.join(sessionToJoin);
+        const message = `[${socket.id}] joined the session [${sessionToJoin}]`;
+        io.in(sessionToJoin).emit('to all clients', message);
+        io.in(sessionToJoin).emit('watch changed', io.sockets.adapter.rooms.get(sessionToJoin).size);
+        log(message);
+
+        if (sessionSettings.has(sessionToJoin)) {
+            if (sessionSettings.get(sessionToJoin)) {
+                const existingSessionSettings = {...sessionSettings.get(sessionToJoin)};
+                if (sessionSettings.get(sessionToJoin).countdownRunning) {
+                    existingSessionSettings.countdownLeft -= Date.now() - existingSessionSettings.timeCountdownStarted;
+                }
+                socket.emit('settings for requested session already exist', existingSessionSettings)
+            }
+        } else {
+            sessionSettings.set(sessionToJoin, {...defaultSessionSettings});
+        }
+    }
+}
+
+function onParticipantsChanged(socket) {
+    return function (participantsChange) {
+        const message = `[${socket.id}] emitted new participants: [${participantsChange.participants}]`;
+        io.in(participantsChange.sessionId).emit('to all clients', message);
+        log(message);
+
+        sessionSettings.get(participantsChange.sessionId).participants = participantsChange.participants;
+
+        socket.broadcast.to(participantsChange.sessionId).emit('participants updated', participantsChange.participants);
+    }
+
+}
+
+function onGoalsChanged(socket) {
+    return function (goalsChange) {
+        const message = `[${socket.id}] emitted new goals: [${goalsChange.goals}]`;
+        io.in(goalsChange.sessionId).emit('to all clients', message);
+        log(message);
+
+        sessionSettings.get(goalsChange.sessionId).goals = goalsChange.goals;
+
+        socket.broadcast.to(goalsChange.sessionId).emit('goals updated', goalsChange.goals);
+    }
+
+}
+
+function onStartStopCountdown() {
+    return function (s) {
+        sessionSettings.get(s.sessionId).countdownRunning = !sessionSettings.get(s.sessionId).countdownRunning;
+        sessionSettings.get(s.sessionId).countdownLeft = s.timeLeft;
+        if (sessionSettings.get(s.sessionId).countdownRunning) {
+            sessionSettings.get(s.sessionId).timeCountdownStarted = Date.now();
+        }
+        io.in(s.sessionId).emit('countdown update', {
+            countdownRunning: sessionSettings.get(s.sessionId).countdownRunning,
+            countdownLeft: sessionSettings.get(s.sessionId).countdownLeft
+        });
+    }
+}
+
+function onResetCountdown() {
+    return function (s) {
+        sessionSettings.get(s.sessionId).countdownLeft = s.timeLeft;
+        io.in(s.sessionId).emit('countdown update', {
+            countdownRunning: sessionSettings.get(s.sessionId).countdownRunning,
+            countdownLeft: sessionSettings.get(s.sessionId).countdownLeft
+        });
+    }
+}
+
+function onCountdownEnded() {
+    return function (s) {
+        sessionSettings.get(s.sessionId).countdownRunning = false;
+        sessionSettings.get(s.sessionId).countdownLeft = s.timeLeft;
+    }
+
+}
+
+function onTimeSecondsSettingsChanged() {
+    return function (s) {
+        sessionSettings.get(s.sessionId).desiredSeconds = s.desiredSeconds;
+        io.in(s.sessionId).emit('seconds changed', {
+            desiredSeconds: s.desiredSeconds
+        });
+    }
+
+}
+
+function onTimeMinutesSettingsChanged() {
+    return function (s) {
+        sessionSettings.get(s.sessionId).desiredMinutes = s.desiredMinutes;
+        io.in(s.sessionId).emit('minutes changed', {
+            desiredMinutes: s.desiredMinutes
+        });
+    }
 }
 
 function stopServer() {
